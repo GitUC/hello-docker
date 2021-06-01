e39f24f9c0738f09c95fd3715c9640646f570b6d
Cathy Zhang (Group) <cathy.zhang@didata.com.au>
2021-06-01 19:40:47 +1000

    add more

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -41,7 +41,7 @@ namespace HelloDocker.Controllers
         [Route("cities")]
         public IActionResult GetCities()
         {
-            return Ok("Sydney, Melbourne, Canberra, Beijing, Nanjing, New York, London");
+            return Ok("Sydney, Melbourne, Canberra, Beijing, Nanjing, New York, London, test ,test");
         }
 
         [HttpGet]
